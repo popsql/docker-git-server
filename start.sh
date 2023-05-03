4bd311e6e36b9c30824af151be687731cf98e5b6
Kevin Kibler <kevin@popsql.com>
2023-05-03 11:40:33 -0400

    POP-6103 - echo known string when server is ready

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 # configure the authorized keys (if any) to allow the client to connect via ssh
 if [ "$SSH_PUBLIC_KEY" ]; then
+  echo 'configuring authorized_keys...'
   cd /home/git
   echo $SSH_PUBLIC_KEY > .ssh/authorized_keys
   chown -R git:git .ssh
@@ -10,11 +11,13 @@ if [ "$SSH_PUBLIC_KEY" ]; then
 fi
 
 # create an empty repo
+echo 'creating repo...'
 cd /home/git
 su git -c "git init  --bare -b main repo"
 
 # use the repo files (if any) to make an initial commit in the repo
 if [ "$(ls -A /root/repoFiles/)" ]; then
+  echo 'creating initial commit...'
   su git -c "git clone repo tmp"
   cp -rT /root/repoFiles tmp
   chown -R git:git tmp
@@ -28,5 +31,9 @@ if [ "$(ls -A /root/repoFiles/)" ]; then
   rm -rf tmp/
 fi
 
+echo 'starting ssh server...'
 # -D flag avoids executing sshd as a daemon
-/usr/sbin/sshd -D
+/usr/sbin/sshd -D &
+
+echo 'git server ready'
+wait $!
